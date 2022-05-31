b9bf8fdc72e20edb80d395da8d5804ea9c265edd
Ilya Bliznyuk <Entrerry@gmail.com>
2022-05-31 22:46:45 +0300

    Update app.rb

--- Lesson3/app.rb
+++ Lesson3/app.rb
@@ -1,58 +1,64 @@
 class Station
+  attr_reader :trains, :title
+
   def initialize(title)
-    @trains_on_station = {} # - Сбда должны попадать {тип поезда => номер поезда}
+    @trains = [] 
     @title = title
   end
 
-  def get_train # - эта функция как будто бы пересекается с функцией из 91 строки, не понимаю как их скомпановать
+  def add_train(train) 
+    @trains << train unless @trains.include?(train)
   end
 
+=begin
   def show_trains_all
-    puts trains_on_station
+    puts trains
+  end
+=end
+
+  def trains_by_type(type)
+    @trains.count { |train| train.type == type }
   end
 
-  def show_trains_type # - тут значения хэша из строки 3 в зависимости от выбраного ключа (freight train или passenger train)
-  end
-
-  def send_train # - не понимаю пока, как реализовать отправку
+  def send_train(train) 
+    @trains.delete(train)
   end
 end
 
 class Route
+  attr_reader :stations, :first, :last
+  
   def initialize(first, last)
     @first = first
     @last = last
-    @route = [first, last]
+    @stations = [first, last]
   end
 
-  def add_station
-    puts 'Which station will be added to the route?'
-    answer = gets.chomp
-    @route.insert(1, "#{answer}")
+  def add_station(station)
+    @stations.insert(1, station)
   end
 
-  def delete_station
-    puts 'Which station will be deleted from the route?'
-    answer = gets.chomp
-    @route.delete("#{answer}")
-
+  def delete_station(station)
+    @stations.delete(station)
   end
 
   def show_route
-    @route.each do |title|
-      #puts "#{title.capitalize}."
+    @stations.each do |station|
+      puts station.title
     end
   end
 end
 
 class Train
+  attr_reader :speed, :type, :railway_carriage_count, :number
+
   def initialize(number, type, railway_carriage_count)
-    #@location = :depot
+    @index_station = 0
     @speed = 0
     @railway_carriage_count = railway_carriage_count
     @number = number
     @type = type
-    puts "We have train num. #{number}. Type is #{type}. And the train has #{railway_carriage_count} railway carriages."
+    #puts "We have train num. #{number}. Type is #{type}. And the train has #{railway_carriage_count} railway carriages."
   end
 
   def speed_up
@@ -71,36 +77,40 @@ class Train
     puts "Train num. #{@number} has #{@railway_carriage_count} railway carriages."
   end
 
-  def change_railway_carriage_count
-    print "We gonna (add) or (delete) railway carriage? "
-    answer = gets.chomp
-    if @speed > 0
-      puts "We cannot change count of railway carriage until train stops!"
-    elsif answer == 'add'
-      @railway_carriage_count += 1 if @speed == 0
-    elsif answer == 'delete'
-      @railway_carriage_count -= 1 if @speed == 0
-    else
-      puts 'I do not understand you!'
-    end
+  def add_railway_carriage
+    @railway_carriage_count += 1 if @speed == 0  
   end
 
-  def get_route #если я верно понимаю, изначально у нас поезд нигде не находится. Первым делом мы придумываем какой-то мршрут, потом расставляем на нём станции и только потом создаём поезда.
+  def delete_railway_carriage
+    @railway_carriage_count -= 1 if @speed == 0
   end
 
-  def move #в одной этой функции, в зависимости от gets, планировал реализовать движение вперёд или назад
+  def get_route(route)
+    @route = route
+  end
+
+  def next_station
+    @route.stations[@index_station + 1]
+  end
+
+  def previous_station
+    @route.stations[@index_station - 1]
   end
 
   def show_location
+    @route.stations[@index_station]
   end
 end
 
+station = Station.new('Depot')
+
 route = Route.new('Depot', 'Moscow')
-puts route.show_route
+puts route.stations
 
 puts ' '
 train1 = Train.new(666, 'passenger train', 5)
-#train1.show_location
+station.add_train(train1)
+puts station.trains
 train1.show_speed
 puts ' '
 train2 = Train.new(667, 'freight train', 20)
